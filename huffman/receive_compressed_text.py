fba2fde94947808681ac34c03d9ddeb3c3341d70
Kevin Fang <kfang1233@gmail.com>
2018-10-20 20:52:18 +0000

    Made interface easier to use

--- huffman/receive_compressed_text.py
+++ huffman/receive_compressed_text.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python3
 import time
 import serial
-import huffman_decode_text
+import decode_text
 
 ser = serial.Serial(
-    '/dev/ttyACM0',
+    '/dev/ttyACM1',
     baudrate=57600,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
@@ -11,11 +12,14 @@ ser = serial.Serial(
     timeout=1)
 
 def decode_input(huff):
-    output = huffman_decode_text.decompress(huff[1:])
+    output = decode_text.decompress(huff[1:])
     length_re = r'(\d+),(.+)'
     import re
     searches = re.findall(length_re, output)[0]
-    return searches[1][:int(searches[0])]
+    if searches != None:
+        return searches[1][:int(searches[0])]
+    else:
+        return None
 
 compr_builder = b''
 while 1:
@@ -30,4 +34,4 @@ while 1:
             print(line)
             compr_builder += line
         else:
-            print("should print now")
+            print(decode_input(compr_builder))
