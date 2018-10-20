2b74d389f8091cbed0cab5edfe1f92433f28c7d1
Shayan <sh.taslim@gmail.com>
2018-10-20 02:40:01 -0400

    updated main

--- MeshProtocol/Controller.py
+++ MeshProtocol/Controller.py
@@ -1,14 +1,14 @@
 from Utilities.NotificationCenter import NotificationCenter as NC
-from queue import Queue
 from uuid import uuid4
 import re
 
-id_queue = Queue(maxsize=10)
-myre = re.compile('^(.{4}):.*$')
+id_queue = list()
+
 
 @NC.notify_on('queue_broadcast')
 def queue_broadcast(data, msg_id):
 
+    myre = re.compile('^(.{4}):.*$')
     if myre.match(data):
         msg_id = myre.findall(data)
         data = data[5:]
@@ -19,6 +19,6 @@ def queue_broadcast(data, msg_id):
     if msg_id in id_queue:
         return
 
-    id_queue.put(msg_id)
+    id_queue.append(msg_id)
     NC.default().post_notification('broadcast', data=data,
                                    msg_id=msg_id)
--- MeshProtocol/main.py
+++ MeshProtocol/main.py
@@ -1,10 +1,12 @@
 from app import create_app, socketio
+from platform import system as st
+import Socket
 
 
 def main():
 
-    app = create_app()
-    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
+    app = create_app(True)
+    socketio.run(app, host='0.0.0.0', port=8080 if st() == 'Darwin' else 80, debug=True)
 
 
 if __name__ == '__main__':
