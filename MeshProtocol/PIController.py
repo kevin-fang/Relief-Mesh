a8e358885c9ed303692178993e0e8071ab637d72
Shayan <sh.taslim@gmail.com>
2018-10-20 16:13:20 -0400

    fix

--- MeshProtocol/PIController.py
+++ MeshProtocol/PIController.py
@@ -29,14 +29,16 @@ def send(text):
 
 
 def start():
-    serial.Serial(
-        '/dev/ttyAMA0',
-        baudrate=57600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1
-    )
+    global ser
+
+    ser = serial.Serial(
+            '/dev/ttyAMA0',
+            baudrate=57600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1
+        )
 
     threading.Thread(target=receive).start()
 
