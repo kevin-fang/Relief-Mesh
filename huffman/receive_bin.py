18fa69522ceecb7ac6c2e791328d94a22227433b
Kevin Fang <kfang1233@gmail.com>
2018-10-20 16:35:42 +0000

    Implemented better receiving

--- huffman/receive_bin.py
+++ huffman/receive_bin.py
@@ -18,28 +18,16 @@ def decode_input(huff):
     return searches[1][:int(searches[0])]
 
 compr_builder = b''
-try:
-    while 1:
-        line = ser.readline()
-        if (line != b''):
-            #print("Received message")
-            #print(line[:-4])
-            if line[0] == ord("~"):
-                #print("start")
-                compr_builder += line[1:]
-                if line[-1] == ord('\n'):
-                    print(decode_input(compr_builder))
-                    compr_builder = b''
-            elif line[-1] != ord("\n"):
-                print(line)
-                compr_builder += line
-            else:
-                print("should print now")
-        #while line[-1] != "|":
-        #    compr_builder += line
-        #    line = ser.readline()
-        #print(huffman_decode_text.decompress(line))
-except KeyboardInterrupt:
-    if compr_builder != b'':
-        print(decode_input(compr_builder))
-
+while 1:
+    line = ser.readline()
+    if (line != b''):
+        if line[0] == ord("~"):
+            compr_builder += line[1:]
+            if line[-1] == ord('\n'):
+                print(decode_input(compr_builder))
+                compr_builder = b''
+        elif line[-1] != ord("\n"):
+            print(line)
+            compr_builder += line
+        else:
+            print("should print now")
